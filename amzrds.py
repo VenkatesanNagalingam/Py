f6e29a28b19d943f045c9c9c2f53dc65027518ab
VenkatesanNagalingam <40203505+VenkatesanNagalingam@users.noreply.github.com>
2018-06-15 20:14:39 +0530

    Update amzrds.py

--- amzrds.py
+++ amzrds.py
@@ -7,17 +7,7 @@ app = flask.Flask(__name__)
 
 @app.route("/")
 def webh():
-  db_username = "root"
-  db_password = "root1234"
-  db_name = "airindiadb"
-  db_endpoint = "airindiapnr.ckbmv5szbwip.us-east-1.rds.amazonaws.com"
-  host=db_endpoint
-  conn = pymysql.connect(host,db_username,db_password, db_name)
-  print(conn)
-  cur=conn.cursor()
-  sql = "select * from tbl_pnr where pnr_number=54321;"
-  data=cur.execute(sql)
-  d=cur.fetchone()
-  return d
+  
+  return "Hello world"
 if __name__ == '__main__':
   app.run()
